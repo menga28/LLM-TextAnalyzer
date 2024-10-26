b4856a111b483138a36c566e90076f8db43bcb8f
Mengascini Mengascini <32158217+menga28@users.noreply.github.com>
2024-10-26 19:20:51 +0200

    Missing fluent configuration

---
+++ app/app.py
@@ -0,0 +1,11 @@
+import time
+
+def main():
+    # Logica principale qui
+    while True:
+        # Puoi mettere qui la logica di elaborazione o le chiamate ai servizi
+        print("Il container è attivo!")
+        time.sleep(60)  # Pausa di 60 secondi
+
+if __name__ == "__main__":
+    main()
