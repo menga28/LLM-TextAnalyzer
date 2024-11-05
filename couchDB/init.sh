33e31a6220583992fe488a8da5c15d1031b68846
Michele Mengascini <menga28@gmail.com>
2024-11-05 17:37:06 +0100

    Update init.sh

--- couchDB/init.sh
+++ couchDB/init.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+# Ottieni l'IP del container di CouchDB
+COUCHDB_IP=$(hostname -i)
+
 # Attende fino a quando CouchDB è disponibile
-until curl -s -o /dev/null http://127.21.0.2:5984; do
+until curl -s -o /dev/null http://$COUCHDB_IP:5984; do
     echo "In attesa di CouchDB..."
     sleep 10
 done
 
 # Crea il database _users se non esiste
 echo "Creazione del database _users..."
-curl -X PUT http://$COUCHDB_USER:$COUCHDB_PASSWORD@127.21.0.2:5984/_users || echo "Il database _users esiste già."
-
-# Aggiungi ulteriori comandi di inizializzazione, se necessari
+curl -X PUT http://$COUCHDB_USER:$COUCHDB_PASSWORD@$COUCHDB_IP:5984/_users || echo "Il database _users esiste già."
