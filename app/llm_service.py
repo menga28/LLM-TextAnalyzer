8860a04e7cc98109f3f1421044c67263c53a522b
Mengascini Mengascini <menga28@gmail.com>
2025-01-17 13:20:19 +0100

    Removed onprem.llm Submodule
    - The onprem.llm submodule has been removed from the repository as it was unnecessary.
    - Instead, the required onprem.llm package has been added to the requirements.txt file for easier installation and dependency management.
    - This change simplifies the project structure and ensures that the dependency is managed directly via standard Python package tools.

--- app/llm_service.py
+++ app/llm_service.py
@@ -1,8 +1,9 @@
 import logging
+import os
 from onprem import LLM
 
-# Configura l'URL del modello LLM
-MODEL_URL = "/app/model/mistral-7b-instruct-v0.2.Q4_K_M.gguf"
+os.environ["AUTO_CONFIRM"] = "true"
+
 
 # Template per il prompt
 TEMPLATE = """
@@ -13,20 +14,30 @@ Below is an instruction that describes a task. Write a response that appropriate
 
 ### Response:"""
 
+
 def process_with_llm(prompt):
     """
     Processa un prompt utilizzando il modello LLM.
-    
+
     Args:
         prompt (str): Il prompt da inviare al modello.
-    
+
     Returns:
-        str: La risposta generata dal modello.
+        str: La risposta generata dal modello.+aa     7
     """
-    # Inizializza il modello
-    llm = LLM(MODEL_URL, n_gpu_layers=-1)
-    
+    llm = LLM(model_url='https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q4_K_M.gguf',
+              prompt_template="<s>[INST] {prompt} [/INST]",
+              confirm=False,
+              n_gpu_layers=-1,
+              temperature=0,
+              verbose=True)
+    llm.download_model(confirm=True)
+
     # Genera la risposta
     response = llm.prompt(prompt, prompt_template=TEMPLATE)
-    
+
     return response
+
+
+def input(prompt=None):
+    return "y"
